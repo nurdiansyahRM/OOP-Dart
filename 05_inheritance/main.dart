d8432ced9530aff2e3908b357f7f0d1e690b7cc1
nurdiansyahRM <nurdiansyahjr46@gmail.com>
2022-11-01 18:38:45 +0700

    changing folder inheritance

--- 05_inheritance/main.dart
+++ 05_inheritance/main.dart
@@ -1,5 +1,8 @@
 import 'Cat.dart';
 void main(){
   var Mycat = Cat ('puspus',5,2,'white');
-  
+ Mycat.Walk();
+ Mycat.eat();
+ print('warna bulu ${Mycat.name} adalah : ${Mycat.furcolor}');
+ print('berat ${Mycat.name} adalah : ${Mycat.weight}');
 }
